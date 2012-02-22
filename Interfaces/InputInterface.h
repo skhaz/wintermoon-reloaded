f92fc5707d4afc16ddba0af2f540cf3f40b94478
Rodrigo Delduca Batista <rodrigodelduca@gmail.com>
2012-02-21 22:37:58 -0200

    A fix I believe, not like I tested or anything

---
+++ Interfaces/InputInterface.h
@@ -0,0 +1,41 @@
+
+/*
+ *                        __
+ *             __        /\ \__
+ *  __  __  __/\_\    ___\ \ ,_\    __  _ __   ___ ___     ___    ___    ___
+ * /\ \/\ \/\ \/\ \ /' _ `\ \ \/  /'__`\\`'__\' __` __`\  / __`\ / __`\/' _ `\
+ * \ \ \_/ \_/ \ \ \/\ \/\ \ \ \_/\  __/ \ \//\ \/\ \/\ \/\ \L\ \\ \L\ \\ \/\ \
+ *  \ \___x___/'\ \_\ \_\ \_\ \__\ \____\ \_\\ \_\ \_\ \_\ \____/ \____/ \_\ \_\
+ *   \/__//__/   \/_/\/_/\/_/\/__/\/____/\/_/ \/_/\/_/\/_/\/___/ \/___/ \/_/\/_/
+ *
+ * Copyright (c) 2006 - 2011 Wintermoon Project
+ *
+ * http://wintermoon.sourceforge.net/
+ *
+ * % BEGIN_LICENSE %
+ * % END_LICENSE %
+ *
+ */
+
+#ifndef _InputInterface_h
+#define _InputInterface_h
+
+#include "Framework/Internal.h"
+
+
+
+WINTERMOON_BEGIN_NAMESPACE
+
+class InputManager;
+class InputInterface
+{
+    public:
+        virtual ~InputInterface();
+
+        virtual void setTarget(InputManager *manager) = 0;
+
+        virtual void update() = 0;
+};
+
+#endif
+
