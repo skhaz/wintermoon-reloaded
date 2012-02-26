549078a230f0424d3b55b9c7ce784c78afe98ade
Rodrigo Delduca Batista <rodrigodelduca@gmail.com>
2012-02-26 18:08:21 -0300

    Added MouseListener

---
+++ Framework/MouseListener.h
@@ -0,0 +1,47 @@
+
+/*
+ *                        __
+ *             __        /\ \__
+ *  __  __  __/\_\    ___\ \ ,_\    __  _ __   ___ ___     ___    ___    ___
+ * /\ \/\ \/\ \/\ \ /' _ `\ \ \/  /'__`\\`'__\' __` __`\  / __`\ / __`\/' _ `\
+ * \ \ \_/ \_/ \ \ \/\ \/\ \ \ \_/\  __/ \ \//\ \/\ \/\ \/\ \L\ \\ \L\ \\ \/\ \
+ *  \ \___x___/'\ \_\ \_\ \_\ \__\ \____\ \_\\ \_\ \_\ \_\ \____/ \____/ \_\ \_\
+ *   \/__//__/   \/_/\/_/\/_/\/__/\/____/\/_/ \/_/\/_/\/_/\/___/ \/___/ \/_/\/_/
+ *
+ * Copyright (c) 2006 - 2011 Wintermoon Project
+ *
+ * http://wintermoon.sourceforge.net/
+ *
+ * % BEGIN_LICENSE %
+ * % END_LICENSE %
+ *
+ */
+
+#ifndef _MouseListener_h
+#define _MouseListener_h
+
+#include "Framework/EventListener.h"
+
+
+
+WINTERMOON_BEGIN_NAMESPACE
+
+class MouseEvent;
+class MouseListener : public EventListener
+{
+    public:
+		MouseListener();
+
+        virtual ~MouseListener();
+
+        virtual void mouseMoveEvent(MouseEvent* event);
+
+        virtual void mouseReleaseEvent(MouseEvent* event);
+
+        virtual void mousePressEvent(MouseEvent* event);
+};
+
+WINTERMOON_END_NAMESPACE
+
+#endif
+
