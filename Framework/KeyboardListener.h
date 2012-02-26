faedcb40cc142c26e8992527e5fbf77dd010c6d4
Rodrigo Delduca Batista <rodrigodelduca@gmail.com>
2012-02-26 18:08:05 -0300

    Added KeyboardListener

---
+++ Framework/KeyboardListener.h
@@ -0,0 +1,46 @@
+
+/*
+ *                        __
+ *             __        /\ \__
+ *  __  __  __/\_\    ___\ \ ,_\    __  _ __   ___ ___     ___    ___    ___
+ * /\ \/\ \/\ \/\ \ /' _ `\ \ \/  /'__`\\`'__\' __` __`\  / __`\ / __`\/' _ `\
+ * \ \ \_/ \_/ \ \ \/\ \/\ \ \ \_/\  __/ \ \//\ \/\ \/\ \/\ \L\ \\ \L\ \\ \/\ \
+ *  \ \___x___/'\ \_\ \_\ \_\ \__\ \____\ \_\\ \_\ \_\ \_\ \____/ \____/ \_\ \_\
+ *   \/__//__/   \/_/\/_/\/_/\/__/\/____/\/_/ \/_/\/_/\/_/\/___/ \/___/ \/_/\/_/
+ *
+ * Copyright (c) 2006 - 2011 Wintermoon Project
+ *
+ * http://wintermoon.sourceforge.net/
+ *
+ * % BEGIN_LICENSE %
+ * % END_LICENSE %
+ *
+ */
+
+#ifndef _KeyboardListener_h
+#define _KeyboardListener_h
+
+#include "Framework/EventListener.h"
+
+
+
+WINTERMOON_BEGIN_NAMESPACE
+
+class KeyEvent;
+class KeyboardListener : public EventListener
+{
+    public:
+		KeyboardListener();
+
+        virtual ~KeyboardListener();
+
+		virtual void keyPressEvent(KeyEvent* event);
+
+		virtual void keyReleaseEvent(KeyEvent* event);
+
+};
+
+WINTERMOON_END_NAMESPACE
+
+#endif
+
