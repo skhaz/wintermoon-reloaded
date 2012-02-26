b3112fca758d9ea585f1e993862ea89e107faade
Rodrigo Delduca Batista <rodrigodelduca@gmail.com>
2012-02-26 19:50:58 -0300

    Resource classes

---
+++ Framework/Resource.cpp
@@ -0,0 +1,33 @@
+/*
+ *                        __
+ *             __        /\ \__
+ *  __  __  __/\_\    ___\ \ ,_\    __  _ __   ___ ___     ___    ___    ___
+ * /\ \/\ \/\ \/\ \ /' _ `\ \ \/  /'__`\\`'__\' __` __`\  / __`\ / __`\/' _ `\
+ * \ \ \_/ \_/ \ \ \/\ \/\ \ \ \_/\  __/ \ \//\ \/\ \/\ \/\ \L\ \\ \L\ \\ \/\ \
+ *  \ \___x___/'\ \_\ \_\ \_\ \__\ \____\ \_\\ \_\ \_\ \_\ \____/ \____/ \_\ \_\
+ *   \/__//__/   \/_/\/_/\/_/\/__/\/____/\/_/ \/_/\/_/\/_/\/___/ \/___/ \/_/\/_/
+ *
+ * Copyright (c) 2006 - 2011 Wintermoon Project
+ *
+ * http://wintermoon.sourceforge.net/
+ *
+ * % BEGIN_LICENSE %
+ * % END_LICENSE %
+ *
+ */
+
+#include "Framework/Resource.h"
+
+
+WINTERMOON_BEGIN_NAMESPACE
+
+Resource::Resource()
+{
+}
+
+Resource::~Resource()
+{
+}
+
+WINTERMOON_END_NAMESPACE
+
---
+++ Framework/ResourceGroup.h
@@ -0,0 +1,39 @@
+/*
+ *                        __
+ *             __        /\ \__
+ *  __  __  __/\_\    ___\ \ ,_\    __  _ __   ___ ___     ___    ___    ___
+ * /\ \/\ \/\ \/\ \ /' _ `\ \ \/  /'__`\\`'__\' __` __`\  / __`\ / __`\/' _ `\
+ * \ \ \_/ \_/ \ \ \/\ \/\ \ \ \_/\  __/ \ \//\ \/\ \/\ \/\ \L\ \\ \L\ \\ \/\ \
+ *  \ \___x___/'\ \_\ \_\ \_\ \__\ \____\ \_\\ \_\ \_\ \_\ \____/ \____/ \_\ \_\
+ *   \/__//__/   \/_/\/_/\/_/\/__/\/____/\/_/ \/_/\/_/\/_/\/___/ \/___/ \/_/\/_/
+ *
+ * Copyright (c) 2006 - 2011 Wintermoon Project
+ *
+ * http://wintermoon.sourceforge.net/
+ *
+ * % BEGIN_LICENSE %
+ * % END_LICENSE %
+ *
+ */
+
+#ifndef _ResourceGroup_h
+#define _ResourceGroup_h
+
+#include "Framework/Internal.h"
+
+
+
+WINTERMOON_BEGIN_NAMESPACE
+
+class ResourceGroup
+{
+    public:
+        ResourceGroup();
+
+        virtual ~ResourceGroup();
+};
+
+WINTERMOON_END_NAMESPACE
+
+#endif
+
